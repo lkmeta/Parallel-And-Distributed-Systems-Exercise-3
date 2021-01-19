a3f502a8617308367cf6a324a2386b2a0edc0a3f
mavioux <pakmarios@gmail.com>
2021-01-19 22:14:27 +0200

    map image from 1D to 2D array

--- src/main2.c
+++ src/main2.c
@@ -13,9 +13,12 @@
 #define MIN_IMAGE_VALUE 0
 #define PI 3.1415926536
 
+double* non_local_means(double* input_image, int patchsize, double filter_sigma, double patch_sigma) {
+
+}
+
 int main() {
     int width, height, bpp;
-    int width2, height2, bpp2;
     time_t t;
 
     /* Intializes random number generator */
@@ -23,15 +26,32 @@ int main() {
 
     uint8_t* original_image = stbi_load("../images/mpezos.jpg", &width, &height, &bpp, CHANNEL_NUM);
     double* normalized_noisy_image = (double *)malloc(width * height * sizeof(double));
+    double** normalized_noisy_2D = (double **)malloc(height * sizeof(double *));
+    for(int i = 0; i < height; i++) {
+        normalized_noisy_2D[i] = (double *)malloc(width * sizeof(double));
+    }
     
     stbi_write_jpg("../images/temp_boat.jpg", width, height, CHANNEL_NUM, original_image, 1);
 
-    uint8_t* noisy_image = stbi_load("../images/temp_boat.jpg", &width2, &height2, &bpp2, CHANNEL_NUM);
+    uint8_t* noisy_image = stbi_load("../images/temp_boat.jpg", &width, &height, &bpp, CHANNEL_NUM);
 
     /* Image Normalization and Noise Addition*/
     for(int i = 0; i < width * height; i++) {
         normalized_noisy_image[i] = (double)noisy_image[i] / (double)(MAX_IMAGE_VALUE - MIN_IMAGE_VALUE + 1);
     }
+
+    /* Map to 2D */
+    for(int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            normalized_noisy_2D[i][j] = normalized_noisy_image[i * width + j];
+        }        
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%f %f\n", normalized_noisy_image[i], normalized_noisy_2D[0][i]);
+    }
+    
 
     stbi_image_free(original_image);
     stbi_image_free(noisy_image);
