8be689dcc04fa4f9b1dcec353bcbc089443f0103
Mavioux <pakmarios@gmail.com>
2021-02-24 11:14:57 +0200

    Updated cuda batch

--- src/batch.sh
+++ src/batch.sh
@@ -7,6 +7,6 @@
 
 module load gcc/7.3.0 cuda/10.0.130
 
-../out/main_cuda
+../out/main_cuda ../images/musk.jpg 3 0.2 1.67
 
 #nvcc -o ../out/main_cuda main_cuda.cu -lm -L/stb_image.h -L/stb_image_write.h
